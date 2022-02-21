043e2240d733ee8f87f2645629cc2d215cf4b72c
Gabriel Roberto de Abreu <gabriel.abreu@ambevtech.com.br>
2022-02-21 15:30:58 -0300

    projeto inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { appRouting } from './app.routing';
+import { MasterLayoutComponent } from './core/layout/master-layout.component';
+import { HomeModule } from './features/home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule, 
+    appRouting,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { routes } from './home.routes';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [HomeComponent]
+})
+export class HomeModule { }
