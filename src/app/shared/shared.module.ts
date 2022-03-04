1be4d47aff469ecba541088db174a878a1ec0fcf
Gabriel Roberto de Abreu <gabriel.abreu@ambevtech.com.br>
2022-03-04 11:17:21 -0300

    changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+
 import { Example01Component } from './charts/example-01/example-01.component';
 import { Example02Component } from './charts/example-02/example-02.component';
+import { Example03Component } from './charts/example-03/example-03.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgxChartsModule
+    NgxChartsModule,
+    NgxEchartsModule
   ],
-  declarations: [Example01Component, Example02Component],
-  exports: [Example01Component, Example02Component]
+  declarations: [Example01Component, Example02Component, Example03Component],
+  exports: [Example01Component, Example02Component, Example03Component]
 })
 export class SharedModule { }
